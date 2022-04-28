37c58754f451ce46712910b3e2be9f08651bd5aa
Harman Sahota <harman.sahota93@gmail.com>
2022-04-28 16:34:25 -0400

    first commit

---
+++ src/main/java/com/harmansahota/tutorialskafkaproducer/TutorialsKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.harmansahota.tutorialkafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialKafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialKafkaProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harmansahota/tutorialskafkaproducer/TutorialsKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harmansahota.tutorialkafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
